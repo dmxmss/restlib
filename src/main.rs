f3853f9b4dbb6ad5d0bbf66853b343036cef14e7
dmx <dexammt@gmail.com>
2024-03-23 18:15:06 +0000

    hello world from file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use std::net::TcpListener;
+use std::{
+    fs,
+    net::{TcpListener, TcpStream}
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
     for stream in listener.incoming() {
-        println!("hello world");
+        let stream = stream.unwrap();
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let contents = fs::read_to_string("index.html").unwrap();
+    let status = "HTTP/1.1 200 OK";
+    let response = format!("{}\r\nContent-Length: {}\r\n\r\n{}", status, contents.len(), contents);
+    stream.write_all(response.as_bytes()).unwrap();
 }
